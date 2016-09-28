2bc689b015956980277cf0fbcb1d4ecc9ada9fcc
Pete Hlawitschka <peter.hlawitschka@gmail.com>
2016-09-28 16:54:17 -0500

    Add NBA data cleaning

--- analysis.R
+++ analysis.R
@@ -16,6 +16,8 @@ require(ggplot2)
 
 
 ###############################################################################
+### NFL DATA
+###############################################################################
 
 # Ranking data
 
@@ -38,7 +40,7 @@ data$roundString = ifelse(data$roundString == 'WildCard', 'Wild Card' ,
 dataTrim <- select(data, winner, loser, roundString, round, year)
 
 
-# New datafram for super bowl winners
+# New dataframe for super bowl winners
 superBowlWinners <- data %>%
                         filter(roundString == 'Super Bowl')
 
@@ -76,7 +78,7 @@ completeResults <- completeResults %>%
                     filter(year != 1982)
 
 
-# Now condense down into a small table
+# Now summarize in a small table describing how often each seed LOST at that round of the playoffs (e.g. that round was the furthest they made it)
 table <- completeResults %>%
     group_by(roundString, seed) %>%
     summarize(count = n())   
@@ -93,3 +95,69 @@ table$target = ifelse(table$target == 1, '1st seed' ,
 # Save table
 write.csv(table, 'nflResults.csv', row.names = F)
 
+
+
+
+###############################################################################
+### NBA DATA
+###############################################################################
+
+nbaData <- read.csv('nbaPlayoffResults.csv', header = T, stringsAsFactors = F)
+
+# Clean 'er up!
+
+    # Check out a couple of weird columns. I think they were just there for aesthetic reasons to make the table display nicely online
+    nrow(nbaData)
+    table(is.na(nbaData$X..1))
+    table(is.na(nbaData$X..2))
+    # Yep, they're always NA. We'll drop them soon...
+    
+    
+    # Rename columns
+    names(nbaData) <- c('year','league','round', 'dates', 'blank','winner','winnerWins','blank2','loser','loserWins')
+    
+    # Drop columns of blanks
+    nbaData <- select(nbaData, year, round, winner, winnerWins, loser, loserWins)
+    # I think there are some rows that aren't data in here still (the column titles are repeated throughout the table for the viewer's reference)
+    table(nbaData$year) # E.g. there are 25 rows where the year == 'Yr'
+    nrow(nbaData)
+    nbaData <-nbaData[nbaData$year != 'Yr',]
+    nrow(nbaData) # Cool. We appear to have dropped 25 rows
+    
+    # To compare to other sports, let's also take just years 1980 and beyond
+    nbaData$year <- as.numeric(nbaData$year)
+    nbaData <- filter(nbaData, year > 1979)
+
+# Extract seeding from the 'winner' and 'loser' columns
+nbaData$winnerSeed <- gsub(".*\\((.*)\\).*", "\\1", nbaData$winner)
+nbaData$loserSeed <- gsub(".*\\((.*)\\).*", "\\1", nbaData$loser)
+nbaData$seed <- nbaData$loserSeed
+
+# Now need to add a row for NBA champions
+nbaChamps <- nbaData %>%
+    filter(round == 'Finals')
+
+# Make some tweaks to this set
+nbaChamps$round <- 'World Champs'
+nbaChamps$seed <- nbaChamps$winnerSeed
+
+
+# Now append the champs data set to the original data
+nbaData <- rbind(nbaData, nbaChamps)
+
+# And lets give the rounds numerical values
+nbaData$roundNum = ifelse(nbaData$round == 'Eastern Conf First Round' | nbaData$round == 'Western Conf First Round', 1,
+                      ifelse(nbaData$round == 'Eastern Conf Semifinals' | nbaData$round == 'Western Conf Semifinals', 2, 
+                             ifelse(nbaData$round == 'Eastern Conf Finals' | nbaData$round == 'Western Conf Finals', 3, 
+                                    ifelse(nbaData$round == 'Finals', 4, 
+                                                  ifelse(nbaData$round == 'World Champs', 5, NA)))))
+
+
+# Now summarize in a small table describing how often each seed LOST at that round of the playoffs (e.g. that round was the furthest they made it)
+nbaSummary <- nbaData %>%
+    group_by(round, seed) %>%
+    summarize(count = n())       
+    
+
+
+View(nbaData %>% filter(round == 'World Champs'))
