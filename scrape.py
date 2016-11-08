8af629680c0105e1be989a69017c5b2dee60c65f
Pete Hlawitschka <peter.hlawitschka@gmail.com>
2016-11-07 19:55:31 -0600

    finish NHL scraping

--- scrape.py
+++ scrape.py
@@ -218,6 +218,16 @@ with open("mlbPlayoffSeeds.csv", "wb") as f:
 
 
 
+
+
+
+
+
+
+
+
+
+
 #########################################################
 ### Scrape hockey-reference for NHL playoff outcomes  ###
 #########################################################
@@ -227,35 +237,37 @@ with open("mlbPlayoffSeeds.csv", "wb") as f:
 os.chdir('H:\playoffRankingData')
 
 rows = []
-year = '2016'
+
 
 # Grab the HTML
-url = 'http://www.baseball-reference.com/postseason/'
+for year in xrange(1994, 2014):
+	if year == 2005:
+		continue
 
-# Scrape the HTML at the url
-r = requests.get(url)
-text = r.text
+	url = 'http://www.hockey-reference.com/playoffs/NHL_' + str(year) +'.html'
+	r = requests.get(url)
+	text = r.text
 
-# Turn the HTML into a Beautiful Soup object
-soup = BeautifulSoup(text, 'lxml')
+	# Turn the HTML into a Beautiful Soup object
+	soup = BeautifulSoup(text, 'lxml')
 
-# Grab the table
-results = soup.find('table', {'class': 'stats_table'})	
+	# Grab the table
+	results = soup.find('table', {'class': 'stats_table'})	
 
-# Grab all of the table cells
-for i in xrange(0,len(results.find_all('tr'))):
-	row = results.find_all('tr')[i]
-	if str(row.find('td', {'class': 'bold_text'})) != 'None':
-		year = row.find('td', {'class': 'bold_text'}).text.encode('utf8').strip()
-	for i in xrange(0, len(row.find_all('td'))):
-		cell = row.find_all('td')[i]
-		value = cell.text.encode('utf8').strip()
-		rows.append(year + ' ' + value)	
-	
-with open('mlbPlayoffResults.csv', 'wb') as f:
-    writer = csv.writer(f)
-    for val in rows:
-        writer.writerow([val])    
+	# Grab all of the table cells
+	for i in xrange(0,len(results.find_all('tr'))):
+		row = results.find_all('tr')[i]
+		if str(row.find('td', {'class': 'bold_text'})) != 'None':
+			year = row.find('td', {'class': 'bold_text'}).text.encode('utf8').strip()
+		for i in xrange(0, len(row.find_all('td'))):
+			cell = row.find_all('td')[i]
+			value = cell.text.encode('utf8').strip()
+			rows.append(year + ' ' + value)	
+		
+	with open('mlbPlayoffResults.csv', 'wb') as f:
+	    writer = csv.writer(f)
+	    for val in rows:
+	        writer.writerow([val])    
 
 ######################################################
 ### Scrape Wikipedia for NHL playoff seeding data  ###
@@ -265,39 +277,86 @@ rows = []
 for year in xrange(1994, 2014):
 	if year == 2005:
 		continue
-	url = 'https://en.wikipedia.org/wiki/'+ str(year) + '_Stanley_Cup_playoffs'
-	# Scrape the HTML at the url
+	url = 'http://www.hockey-reference.com/playoffs/NHL_' + str(year) +'.html'
 	r = requests.get(url)
 	text = r.text
+
 	# Turn the HTML into a Beautiful Soup object
 	soup = BeautifulSoup(text, 'lxml')
 
-	# Wikipedia lists the playoff seeding in ordered-list divs
-	playoffTeams = soup.findAll('ol')
-	try:	
-		# Eastern conf
-		cells = [val.text.encode('utf8') for val in playoffTeams[0].find_all(['li'])]
-		for j in xrange(0, len(cells)):
-			team = [cells[j]]
-			team.append(year)
-			team.append(j+1)
-			team.append('Eastern')
-			rows.append(team)
-		# Eastern conf
-		cells = [val.text.encode('utf8') for val in playoffTeams[1].find_all(['li'])]
-		for j in xrange(0, len(cells)):
-			team = [cells[j]]
-			team.append(year)
-			team.append(j+1)
-			team.append('Western')
-			rows.append(team)
-	except:
-		print year
-	continue    
+	# Grab the table
+	# Set recursive = F in find_all, because there are some rows you can un-collapse for more details and we DON'T want those
+	results = soup.find('table', {'id': 'all_playoffs'}).find('tbody').find_all('tr', class_=lambda x: x != 'toggleable', recursive=False)
+
+	# Grab all of the table cells
+	for i in xrange(0,len(results)):
+		# Find all of the td cells in that row
+		row = results[i].find_all('td')
+		# And throw them all into a list, so td can be put in its own column
+		data = []
+		for j in xrange(0, len(row)):
+			cell = row[j]
+			value = cell.text.encode('utf8').strip()
+			data.append(value)
+		data.append(year)
+		rows.append(data)
+
+with open('nhlPlayoffResults.csv', 'wb') as f:
+    writer = csv.writer(f)
+    for val in rows:
+        writer.writerow([val]) 
 
 
-# Write to a CSV file
-with open("nhlPlayoffSeeds.csv", "wb") as f:
-	writer = csv.writer(f)
-	writer.writerows(rows)
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rows = []
+year = 1992
+
+url = 'http://www.hockey-reference.com/playoffs/NHL_' + str(year) +'.html'
+r = requests.get(url)
+text = r.text
+
+# Turn the HTML into a Beautiful Soup object
+soup = BeautifulSoup(text, 'lxml')
+
+# Grab the table
+# Set recursive = F in find_all, because there are some rows you can un-collapse for more details and we DON'T want those
+results = soup.find('table', {'id': 'all_playoffs'}).find('tbody').find_all('tr', class_=lambda x: x != 'toggleable', recursive=False)
+
+# Grab all of the table cells
+for i in xrange(0,len(results)):
+	# Find all of the td cells in that row
+	row = results[i].find_all('td')
+	# And throw them all into a list, so td can be put in its own column
+	data = []
+	for j in xrange(0, len(row)):
+		cell = row[j]
+		value = cell.text.encode('utf8').strip()
+		data.append(value)
+	data.append(year)
+	rows.append(data)
+
+with open('nhlPlayoffResults.csv', 'wb') as f:
+    writer = csv.writer(f)
+    for val in rows:
+        writer.writerow([val]) 
